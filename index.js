834e3addbd09ef6c1dca862d29d0e11fa531b868
Harsh Kumar Sharma <hk.harshkumar11@gmail.com>
2023-01-30 07:04:43 +0530

    insta push

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const SocialPost = require("social-post-api");
+const social = new SocialPost('JG9E1HP-FFGMVTV-P621DAC-1Y5P09V');
 
 const app = express();
 
@@ -6,9 +8,29 @@ const port =9000;
 
 
 app.use("/",(req,res)=>{
+    run();
     res.json({message:"Hello from express app"})
 })
 
+const run = async () => {
+    /** post */
+    const post = await social.post({
+      post: "Who you gonna call?",
+      platforms: ["instagram"],
+      mediaUrls: ["https://img.ayrshare.com/012/gb.jpg"]
+    }).catch(console.error);
+    console.log(post);
+  
+    /** history */
+    // const history = await social.history()
+    //    .catch(console.error);
+    // console.log(history);
+  
+    // /** delete */
+    // const deletePost = await social.delete({ id: post.id })
+    //    .catch(console.error);
+    // console.log(deletePost);
+  };
 
 app.listen(port, ()=>{
     console.log(`Running server on port ${port}`)
