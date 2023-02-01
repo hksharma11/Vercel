551d16175bba28ac650af60bf918eb68cefcc769
Harsh Kumar Sharma <hk.harshkumar11@gmail.com>
2023-02-02 03:46:25 +0530

    changes

--- index.js
+++ index.js
@@ -1,37 +1,37 @@
 const express = require('express');
 const SocialPost = require("social-post-api");
-const social = new SocialPost('JG9E1HP-FFGMVTV-P621DAC-1Y5P09V');
 
 const app = express();
 
-const port =9000;
+const port = 9000;
 
 
-app.use("/",(req,res)=>{
-    run();
-    res.json({message:"Hello from express app"})
+app.use("/data/", (req, res) => {
+    run(req.query.Key,req.query.Media,req.query.Caption);
+    res.send("Ok")
 })
 
-const run = async () => {
+const run = async (key,media,caption) => {
     /** post */
+    const social = new SocialPost(key);
     const post = await social.post({
-      post: "Who you gonna call?",
-      platforms: ["instagram"],
-      mediaUrls: ["https://img.ayrshare.com/012/gb.jpg"]
+        post: caption,
+        platforms: ["instagram"],
+        mediaUrls: [media]
     }).catch(console.error);
     console.log(post);
-  
+
     /** history */
     // const history = await social.history()
     //    .catch(console.error);
     // console.log(history);
-  
+
     // /** delete */
     // const deletePost = await social.delete({ id: post.id })
     //    .catch(console.error);
     // console.log(deletePost);
-  };
+};
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`Running server on port ${port}`)
 })
